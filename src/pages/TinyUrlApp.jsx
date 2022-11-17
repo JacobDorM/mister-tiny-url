4a3ce2d046f8d5772de8ec75cc26d7637505bdf1
Jacob Menachem <dorlabor1@gmail.com>
2022-11-17 11:05:14 +0200

    feat: TinyUrl form setup

--- src/pages/TinyUrlApp.jsx
+++ src/pages/TinyUrlApp.jsx
@@ -1,3 +1,23 @@
+import { useForm } from '../customHooks/useForm'
+import { utilService } from '../services/utilService.js'
+
 export const TinyUrlApp = () => {
-  return <div className="tinyUrl-app">tinyUrl</div>
+  const [url, handleChange] = useForm('', () => {
+    console.log(url)
+  })
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    !utilService.isValidHttpUrl(url.longUrl) ? console.log('Invalid URl') : console.log('Valid URL')
+  }
+
+  return (
+    <div className="tinyUrl-app">
+      <form onSubmit={onSubmit} className="tinyUrl-form">
+        <label htmlFor="longUrl">Enter a long URL to make a TinyUrl</label>
+        <input type="text" onChange={handleChange} name="longUrl" id="longUrl" />
+        <button>Make TinyUrl!</button>
+      </form>
+    </div>
+  )
 }
